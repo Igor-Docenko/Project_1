7f6fc0fed40f788774d4aceeea33d7b5d753c682
Igor-Docenko <zeppelin.agent44@gmail.com>
2024-06-06 00:46:24 +0300

    Версия № 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Всем привет и хорошего настроения");
-        System.out.println("До новых встреч");
+        System.out.println("До новых ");
     }
 }
